6bd10f5277d9f0d1cfcc59f1dee9061f4357bf37
zhh <eric@handmobi.com>
2019-07-02 15:50:34 +0800

    工程

---
+++ docker-demo/src/main/java/com/zhh/docker/dockerdemo/controller/Controller.java
@@ -0,0 +1,18 @@
+package com.zhh.docker.dockerdemo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author:zhh
+ * @Date:Created in 9:59 2019/6/24 0024
+ */
+@RestController
+public class Controller {
+
+    @RequestMapping("/test")
+    public String test(){
+        System.out.println("docker test");
+        return "docker-demo";
+    }
+}
